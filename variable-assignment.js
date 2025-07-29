5904ae84fe923e1307c03dd7c454d101f1214516
atulbisale <atulbisale@gmail.com>
2025-07-29 10:00:18 +0530

    variable-assignment
    Variable Assignment

---
+++ variable-assignment.js
@@ -0,0 +1,31 @@
+
+// Variable Declaration
+var cityName;
+//Variable Initialization
+cityName = "Sangola"; 
+console.log("My City Name is: " + cityName);
+
+// Variable Declaration
+var friendName;
+//Variable Initialization
+friendName = "Sagar"; 
+console.log("My Best Friend Name is: " + friendName);
+
+// Variable Declaration
+var countryName;
+//Variable Initialization
+countryName = "India"; 
+console.log("My Country Name is: " + countryName);
+
+// Variable Declaration
+var schoolName;
+//Variable Initialization
+schoolName = "New English School, Sangola"; 
+console.log("My School Name is: " + schoolName);
+
+// Variable Declaration
+var collegeName;
+//Variable Initialization
+collegeName = "Sangola college, Sangola"; 
+console.log("My College Name is: " + collegeName);
+
