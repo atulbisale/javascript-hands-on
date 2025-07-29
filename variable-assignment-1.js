e800de3fa1ee84624ec41d480ce345b17ceba3df
atulbisale <atulbisale@gmail.com>
2025-07-29 10:01:54 +0530

    variable-assignment-1
    Variable Assignment 1

---
+++ variable-assignment-1.js
@@ -0,0 +1,46 @@
+
+// Declare variable
+var myFullName;
+console.log("My full name Value before initialization: "+ myFullName);
+
+// Initialization variable
+myFullName = " Atul Bisale"
+console.log("My full Name is: " + myFullName);
+
+// Updated variable value
+myFullName = "Atul Agatrao Bisale"
+console.log("My full Name is: " + myFullName);
+console.log("-----------------------------------------------------------------");
+
+// Declare And Initialize a variable
+var myLove;
+myLove = "I love only JS"
+console.log("My love: " + myLove);
+
+// Declare and initialize vaiable on same line
+var firstName = "Atul"
+console.log("My first name is: " +firstName);
+
+var lastName = "Atul"
+console.log("My last name is: " +lastName);
+
+var age = "25"
+console.log("My age is: " +age);
+
+var address = ("A/P- Vasud Road, Narendra Nagar, Sangola, Dist-Solapur");
+console.log("My Address is: " +address);
+
+var pinCode = ("413307");
+console.log("My pin Code: " +pinCode);
+
+var gender = ("Male");
+console.log("My gender is: " +gender);
+
+var isMarried = ("No");
+console.log("Is married: " +isMarried);
+
+
+
+
+
+
